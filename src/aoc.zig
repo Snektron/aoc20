90221ebd9206aadca69dd1c06a63a193231a5d73
Robin Voetter <robin@voetter.nl>
2020-12-01 19:20:28 +0100

    Make day 1, part 1 linear

--- src/aoc.zig
+++ src/aoc.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
+const Log2Int = std.math.Log2Int;
+const Int = std.meta.Int;
 
 pub fn splitToIntegers(
     comptime T: type,
     allocator: *std.mem.Allocator,
     data: []const u8,
     delim: []const u8
-) ![]const T {
+) ![]T {
     var items = std.ArrayList(T).init(allocator);
     errdefer items.deinit();
 
@@ -18,6 +20,63 @@ pub fn splitToIntegers(
     }
 
     return items.toOwnedSlice();
+}
+
+fn radix(comptime T: type, comptime bucket_bits: Log2Int(T), shift: Log2Int(T), value: T) Int(.unsigned, bucket_bits) {
+    const RadixType = Int(.unsigned, bucket_bits);
+    return @intCast(RadixType, (value >> shift) & std.math.maxInt(RadixType));
+}
+
+fn partialRadixSort(comptime T: type, comptime bucket_bits: Log2Int(T), shift: Log2Int(T), src: []const T, dst: []T) void {
+    std.debug.assert(src.len == dst.len);
+    std.debug.assert(bucket_bits > 0);
+    const buckets = 1 << bucket_bits;
+
+    // Calculate the initial number of elements in each bucket
+    var prefix = [_]usize{0} ** buckets;
+    for (src) |elem| {
+        prefix[radix(T, bucket_bits, shift, elem)] += 1;
+    }
+
+    // Calculate the offset in `dst` for each bucket
+    var accum: usize = 0;
+    for (prefix) |*elem| {
+        const tmp = accum + elem.*;
+        elem.* = accum;
+        accum = tmp;
+    }
+
+    // Sort the items into the buckets
+    for (src) |elem| {
+        const bucket = radix(T, bucket_bits, shift, elem);
+        dst[prefix[bucket]] = elem;
+        prefix[bucket] += 1;
+    }
+}
+
+pub fn radixSort(comptime T: type, comptime bucket_bits: Log2Int(T), src: []T, tmp: []T) void {
+    const steps = @sizeOf(T) * 8 / @as(usize, bucket_bits);
+    std.debug.assert(steps * bucket_bits == @sizeOf(T) * 8);
+
+    var i: usize = 0;
+    while (i < steps) : (i += 1) {
+        const shift = @intCast(Log2Int(T), i * bucket_bits);
+        if (i % 2 == 0) {
+            partialRadixSort(T, bucket_bits, shift, src, tmp);
+        } else {
+            partialRadixSort(T, bucket_bits, shift, tmp, src);
+        }
+    }
+
+    if (steps % 2 == 1) {
+        std.mem.copy(T, src, tmp);
+    }
+}
+
+pub fn radixSortAlloc(comptime T: type, comptime bucket_bits: Log2Int(T), allocator: *Allocator, src: []T) !void {
+    const tmp = try allocator.alloc(T, src.len);
+    defer allocator.free(tmp);
+    radixSort(T, bucket_bits, src, tmp);
 }
 
 pub fn print(comptime fmt: []const u8, args: anytype) void {
--- src/day01.zig
+++ src/day01.zig
@@ -7,23 +7,34 @@ pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = &arena.allocator;
-    const list = try aoc.splitToIntegers(u64, allocator, input, "\n");
+    const list = try aoc.splitToIntegers(u16, allocator, input, "\n");
 
-    aoc.print("Part 1\n", .{});
-    for (list[0 .. list.len - 1]) |a, i| {
-        for (list[i + 1..]) |b| {
-            if (a + b == 2020) {
-                aoc.print("{} * {} = {}\n", .{ a, b, a * b });
+    try aoc.radixSortAlloc(u16, 4, allocator, list);
+
+    {
+        aoc.print("Part 1:\n", .{});
+        var i: usize = 0;
+        var j: usize = list.len - 1;
+
+        while (i != j) {
+            const sum = list[i] + list[j];
+            if (sum == 2020) {
+                aoc.print("{} * {} = {}\n", .{ list[i], list[j], @as(usize, list[i]) * list[j] });
+                break;
+            } else if (sum > 2020) {
+                j -= 1;
+            } else {
+                i += 1;
             }
         }
     }
 
-    aoc.print("Part 2\n", .{});
-    for (list[0 .. list.len - 2]) |a, i| {
-        for (list[i + 1 .. list.len - 1]) |b, j| {
-            for (list[j + 1 ..]) |c| {
+    aoc.print("Part 2:\n", .{});
+    for (list[2 ..]) |a, i| {
+        for (list[1 .. i + 2]) |b, j| {
+            for (list[0 .. j + 1]) |c| {
                 if (a + b + c == 2020) {
-                    aoc.print("{} * {} * {} = {}\n", .{ a, b, c, a * b * c });
+                    aoc.print("{} * {} * {} = {}\n", .{ a, b, c, @as(usize, a) * b * c });
                 }
             }
         }
