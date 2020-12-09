bffaba78072e2b0b494fb6555f60b3b7e339b219
Robin Voetter <robin@voetter.nl>
2020-12-09 04:09:28 +0100

    Day 08, O(1) memory

--- src/day08.zig
+++ src/day08.zig
@@ -1,62 +1,90 @@
 const std = @import("std");
-const input = @import("input/08.zig").input;
+const in = @import("input/08.zig");
 const aoc = @import("aoc.zig");
+const Instruction = in.Instruction;
 
-const RunResult = struct {
-    terminates: bool,
+const State = struct {
     acc: i32,
+    i: usize,
+
+    const initial = State{.acc = 0, .i = 0};
+
+    fn next(self: State, input: []const Instruction) State {
+        const inst = input[self.i];
+        const acc = switch (inst.opcode) {
+            .acc => self.acc + inst.arg,
+            else => self.acc,
+        };
+
+        const i = switch (inst.opcode) {
+            .acc, .nop => self.i + 1,
+            .jmp => if (inst.arg < 0)
+                    self.i - @intCast(u32, -inst.arg)
+                else
+                    self.i + @intCast(u32, inst.arg),
+        };
+
+        return .{.acc = acc, .i = i};
+    }
 };
 
-fn run(override: ?usize) RunResult {
-    var seen = [_]bool{false} ** input.len;
+const FcfResult = struct {
+    start: usize,
+    period: usize,
+    final_state: State,
+};
 
-    var i: usize = 0;
-    var acc: i32 = 0;
-    while (!seen[i]) {
-        seen[i] = true;
-        var inst = input[i];
-        if (override != null and override.? == i) {
-            inst.opcode = switch (inst.opcode) {
-                .acc => unreachable,
-                .nop => .jmp,
-                .jmp => .nop,
-            };
-        }
+fn fcf(start: State, input: []const Instruction) FcfResult {
+    var t = start.next(input);
+    var h = t.next(input);
 
-        switch (inst.opcode) {
-            .acc => { acc += inst.arg; i += 1; },
-            .nop => i += 1,
-            .jmp => if (inst.arg < 0) {
-                i -= @intCast(u32, -inst.arg);
-            } else {
-                i += @intCast(u32, inst.arg);
-            },
-        }
-
-        if (i == input.len) {
-            return .{
-                .terminates = true,
-                .acc = acc,
-            };
-        }
+    while (t.i != h.i) {
+        t = t.next(input);
+        h = h.next(input).next(input);
     }
 
-    return .{
-        .terminates = false,
-        .acc = acc,
-    };
+    h = start;
+    var i: usize = 0;
+    while (t.i != h.i) : (i += 1) {
+        t = t.next(input);
+        h = h.next(input);
+    }
+
+    h = h.next(input);
+    var j: usize = 1;
+    while (t.i != h.i) : (j += 1) {
+        h = h.next(input);
+    }
+
+    return .{.start = i, .period = j, .final_state = h};
 }
 
 pub fn main() void {
-    aoc.print("Day 08, part 01: {}\n", .{ run(null).acc });
+    var input = in.input;
+    const fcf_result = fcf(State.initial, &input);
+    aoc.print("Day 08, part 01: {}\n", .{ fcf_result.final_state.acc });
 
-    for (input) |inst, i| {
-        if (inst.opcode == .acc) continue;
+    const max = fcf_result.start + fcf_result.period;
 
-        const result = run(i);
-        if (result.terminates) {
-            aoc.print("Day 08, part 02: {}\n", .{ result.acc });
-            break;
+    for (input) |*inst, i| {
+        const orig = inst.opcode;
+        inst.opcode = switch (orig) {
+            .acc => continue,
+            .jmp => .nop,
+            .nop => .jmp,
+        };
+
+        var state = State.initial;
+        var j: usize = 0;
+        while (j <= max) : (j += 1) {
+            state = state.next(&input);
+
+            if (state.i == input.len) {
+                aoc.print("Day 08, part 02: {}\n", .{ state.acc });
+                return;
+            }
         }
+
+        inst.opcode = orig;
     }
 }
